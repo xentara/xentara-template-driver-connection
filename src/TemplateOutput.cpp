b3b3852cf007067f53adbaceeb639a07abdeda14
Georg Schwab <georg.schwab@xentara.io>
2023-10-02 11:01:01 +0200

    Updated for the latest developer preview

--- src/TemplateOutput.cpp
+++ src/TemplateOutput.cpp
@@ -22,18 +22,12 @@ namespace xentara::plugins::templateDriver
 	
 using namespace std::literals;
 
-TemplateOutput::Class TemplateOutput::Class::_instance;
-
 const model::Attribute TemplateOutput::kValueAttribute { model::Attribute::kValue, model::Attribute::Access::ReadWrite, data::DataType::kFloatingPoint };
 
-auto TemplateOutput::loadConfig(const ConfigIntializer &initializer,
-		utils::json::decoder::Object &jsonObject,
-		config::Resolver &resolver,
-		const config::FallbackHandler &fallbackHandler) -> void
+auto TemplateOutput::load(utils::json::decoder::Object &jsonObject,
+	config::Resolver &resolver,
+	const config::FallbackHandler &fallbackHandler) -> void
 {
-	// Get a reference that allows us to modify our own config attributes
-    auto &&configAttributes = initializer[Class::instance().configHandle()];
-
 	// Go through all the members of the JSON object that represents this object
 	for (auto && [name, value] : jsonObject)
     {
@@ -50,7 +44,7 @@ auto TemplateOutput::loadConfig(const ConfigIntializer &initializer,
 				utils::json::decoder::throwWithLocation(value, std::runtime_error("TODO is wrong with TODO parameter of template output"));
 			}
 
-			/// @todo set the appropriate member variables, and update configAttributes accordingly (if necessary) 
+			/// @todo set the appropriate member variables
 		}
 		else
 		{
